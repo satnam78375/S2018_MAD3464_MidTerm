14d6da6e7f4f0b520ae1c07f00a7ca439e383cca
satnamsidhu425@gmail.com <satnamsidhu425@gmail.com>
2018-06-07 17:02:36 -0700

    binaryprogram

--- c738040MidTerm_MADS3464/src/c738040midterm_mads3464/Binarytodecimal.java
+++ c738040MidTerm_MADS3464/src/c738040midterm_mads3464/Binarytodecimal.java
@@ -10,5 +10,22 @@ package c738040midterm_mads3464;
  * @author satnam
  */
 public class Binarytodecimal {
-    
+  public static int convert(String input) {
+        Long binary =  Long.valueOf(input).longValue();
+        int decimalNumber = 0, i = 0;
+        long remainder;
+        while (binary != 0)
+        {
+            remainder = binary % 10;
+            binary /= 10;
+            decimalNumber += remainder * Math.pow(2, i);
+            ++i;
+        }
+        return decimalNumber;
+    }
 }
+ 
+
+
+
+
